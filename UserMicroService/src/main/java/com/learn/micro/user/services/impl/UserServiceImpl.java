8e9032af6ba75f49222afe6427896a32ffb291a4
Manoj Patidar <35284625+manojpatidar0505@users.noreply.github.com>
2023-01-24 22:45:02 +0530

    API Comm via resttemplate

--- UserMicroService/src/main/java/com/learn/micro/user/services/impl/UserServiceImpl.java
+++ UserMicroService/src/main/java/com/learn/micro/user/services/impl/UserServiceImpl.java
@@ -1,12 +1,17 @@
 package com.learn.micro.user.services.impl;
 
+import com.learn.micro.user.entity.Rating;
 import com.learn.micro.user.entity.User;
 import com.learn.micro.user.exception.ResourceNotFoundException;
 import com.learn.micro.user.repository.UserRepository;
 import com.learn.micro.user.services.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -15,6 +20,11 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserRepository repository;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Override
     public User saveUser(User user) {
@@ -30,6 +40,11 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User getUser(String userId) {
-        return repository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("Record Not found against requested id "+userId));
+        User user = repository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("Record Not found against requested id " + userId));
+        String url = "http://localhost:8083/ratings/users/" + userId;
+        ArrayList<Rating> list = restTemplate.getForObject(url, ArrayList.class);
+        logger.info("Ratings:{}", list.toString());
+        user.setRatings(list);
+        return user;
     }
 }
