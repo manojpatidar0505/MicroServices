2944dc2b9ca60c2052d3a63869a65d354fc2bdbd
Manoj Patidar <35284625+manojpatidar0505@users.noreply.github.com>
2023-01-24 21:40:02 +0530

    Eureka Server Added

---
+++ ServiceRegistory/src/test/java/com/learn/micro/serviceregistory/ServiceRegistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.micro.serviceregistory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
