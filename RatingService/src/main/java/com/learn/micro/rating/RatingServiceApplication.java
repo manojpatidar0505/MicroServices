7f5d67d5e3db2e0f219e3433d8dc7f57be91873b
Manoj Patidar <35284625+manojpatidar0505@users.noreply.github.com>
2023-01-24 22:44:14 +0530

    Enable Eureka Client for Rating Service

--- RatingService/src/main/java/com/learn/micro/rating/RatingServiceApplication.java
+++ RatingService/src/main/java/com/learn/micro/rating/RatingServiceApplication.java
@@ -2,12 +2,14 @@ package com.learn.micro.rating;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class RatingServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RatingServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RatingServiceApplication.class, args);
+    }
 
 }
